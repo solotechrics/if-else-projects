ec3f64f1e9811a014d15bf9ff186e4c996caa966
Solomon Opeyemi <solotechrics@gmail.com>
2024-12-21 16:14:41 +0100

    Add style to the webApp

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const numberInput = document.querySelector(".numberInput");
 const numberChecker = document.querySelector(".numberChecker");
-const btn = document.querySelector(".btn");
-const num = Number(numberInput.value);
+const btn = document.querySelector(".btn")
+btn.addEventListener("click", Checker);
+
+function Checker(){
+    const num = Number(numberInput.value);
+
+    if(num % 2 === 0){
+        numberChecker.textContent = `${num}: This is an Even Number`;
+    } else {
+        numberChecker.textContent = `${num}: This is an Odd Number`;
+    }
+}
