5f7da55374d988bf4fc5e4d2f3ee90244f28cfca
Solomon Opeyemi <solotechrics@gmail.com>
2024-12-21 13:42:37 +0100

    Create html platform for user input of number

---
+++ script.js
@@ -0,0 +1,4 @@
+const numberInput = document.querySelector(".numberInput");
+const numberChecker = document.querySelector(".numberChecker");
+const btn = document.querySelector(".btn");
+const num = Number(numberInput.value);
