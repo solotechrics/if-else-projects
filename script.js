44d586d0da03e36c61ffca6924b42444e53143d9
Solomon Opeyemi <solotechrics@gmail.com>
2024-12-21 12:32:06 +0100

    Create function handler for age validation

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const ageInput = document.querySelector(".ageInput");
 const output = document.querySelector(".output");
 const btn = document.querySelector(".btn");
+
+// Age Validation
+const ageValidation = 18;
+
+btn.addEventListener("click", checkAge());
+function checkAge(){
+    const age = Number(ageInput.value);
+
+    if(age >= ageValidation){
+        output.textContent = `${age}:✅ You are elgible to drive`;
+    } else if (age < ageValidation) {
+        output.textContent = `${age}: ❌ Sorry you are not elgible to drive`;
+    } else {
+        output.textContent = "Invalid input";
+    }
+}
