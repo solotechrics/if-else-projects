31addf5570f2cb14a899cb6e851d640be169252b
Solomon Opeyemi <solotechrics@gmail.com>
2024-12-24 11:10:20 +0100

    Correct typo erros in class deceration

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const userInputs = document.querySelector(".scoreInput");
 const outputs = document.querySelector(".output");
-const submitBtn = document.querSelector(".gradeSubmit");
 const submit = document.querySelector(".gradeSubmit")
 
 submit.addEventListener("click", gradeCalculator);
@@ -8,7 +7,7 @@ submit.addEventListener("click", gradeCalculator);
 function gradeCalculator(){
   const input = parseFloat(userInputs.value);
 
-  if(input === 90 || input >= 90){
+  if(input >= 90){
     outputs.textContent = `${input}: Your Grade is A`;
   }
 }
