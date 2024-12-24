77e97d75c1e592dad449453450a9ee5601ba9e70
Solomon Opeyemi <solotechrics@gmail.com>
2024-12-24 11:04:28 +0100

    create test grade for A:

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const input = document.querySelector(".scoreInput");
-const output = document.querySelector(".output");
+const userInputs = document.querySelector(".scoreInput");
+const outputs = document.querySelector(".output");
 const submitBtn = document.querSelector(".gradeSubmit");
 const submit = document.querySelector(".gradeSubmit")
 
 submit.addEventListener("click", gradeCalculator);
+
+function gradeCalculator(){
+  const input = parseFloat(userInputs.value);
+
+  if(input === 90 || input >= 90){
+    outputs.textContent = `${input}: Your Grade is A`;
+  }
+}
