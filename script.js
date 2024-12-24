8c13afdee4509e944b1ea39d23b7927e3b9d5daa
Solomon Opeyemi <solotechrics@gmail.com>
2024-12-24 10:55:41 +0100

    Link all html classes to javaScript for user handler

---
+++ script.js
@@ -0,0 +1,6 @@
+const input = document.querySelector(".scoreInput");
+const output = document.querySelector(".output");
+const submitBtn = document.querSelector(".gradeSubmit");
+const submit = document.querySelector(".gradeSubmit")
+
+submit.addEventListener("click", gradeCalculator);
