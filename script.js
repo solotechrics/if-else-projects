e7b58cfd986f8dba93909ea9a788a19ccdb4f471
Solomon Opeyemi <solotechrics@gmail.com>
2024-12-24 11:22:25 +0100

    Add statement for other grades

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const userInputs = document.querySelector(".scoreInput");
 const outputs = document.querySelector(".output");
-const submit = document.querySelector(".gradeSubmit")
+const submit = document.querySelector(".gradeSubmit");
 
 submit.addEventListener("click", gradeCalculator);
 
-function gradeCalculator(){
+function gradeCalculator() {
   const input = parseFloat(userInputs.value);
 
-  if(input >= 90){
+  if (input >= 90) {
     outputs.textContent = `${input}: Your Grade is A`;
+  } else if (input >= 80) {
+    outputs.textContent = `${input}: Your Grade is B`;
+  } else if (input >= 70) {
+    outputs.textContent = `${input}: Your Grade is C`;
+  } else if (input >= 60) {
+    outputs.textContent = `${input}: Your Grade is D`;
+  } else {
+    outputs.textContent = `${input}: Your Grade is F`;
   }
 }
