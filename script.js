d9307e2382db416ad4e5e3e2eb5b54d238e9c564
Solomon Opeyemi <solotechrics@gmail.com>
2024-12-26 04:45:51 +0100

    Add additional styles to the webApp to make it user Friendly

--- script.js
+++ script.js
@@ -6,15 +6,16 @@ submit.addEventListener("click", gradeCalculator);
 
 function gradeCalculator() {
   const input = parseFloat(userInputs.value);
+  outputs.className ="";
 
   // Validate input
   if(isNaN(input)){
-    outputs.textContent = "please enter a valid numer.";
+    outputs.textContent = "please enter a valid number.";
     return;
   }
 
   if (input < 0 || input > 100){
-    outputs.textContent = "please enter a score between o and 100";
+    outputs.textContent = "please enter a score between 0 and 100";
     return;
   }
 
@@ -22,13 +23,22 @@ function gradeCalculator() {
 
   if (input >= 90) {
     outputs.textContent = `${input}: Your Grade is A`;
+    outputs.classList.add("gradeA");
   } else if (input >= 80) {
     outputs.textContent = `${input}: Your Grade is B`;
+    outputs.classList.add("gradeB");
+
   } else if (input >= 70) {
     outputs.textContent = `${input}: Your Grade is C`;
+    outputs.classList.add("gradeC");
+
   } else if (input >= 60) {
     outputs.textContent = `${input}: Your Grade is D`;
+    outputs.classList.add("gradeD");
+
   } else {
     outputs.textContent = `${input}: Your Grade is F`;
+    outputs.classList.add("gradeF");
+
   }
 }
